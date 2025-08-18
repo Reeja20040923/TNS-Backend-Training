ddd0876e53c3aa852286f7772210948f0514a188
Reeja20040923 <reejayaramalla@gmail.com>
2025-08-18 22:13:37 +0530

    Inheritance Demo2

---
+++ arrays/src/com/reeja/arrays/Main.java
@@ -0,0 +1,25 @@
+package com.reeja.arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Box box = new Box(4);
+        Box box1 = new Box(4.6,7.9,9.9);
+
+        System.err.println(box.l+" "+box.w+" "+box.h);
+        System.err.println(box1.l+" "+box1.w+" "+box1.h);
+        
+        
+        
+
+
+    }
+    
+}
+
+
+
+	
+
+	
+
+
